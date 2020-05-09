2e50c0f5a3a6a9d0829dfe832b329aae78b1d5ec
TechMaster980 <37854546+TechMaster980@users.noreply.github.com>
2020-05-09 12:59:58 -0700

    finish one

--- src/_01_race_car/RaceCarRunner.java
+++ src/_01_race_car/RaceCarRunner.java
@@ -1,4 +1,7 @@
 package _01_race_car;
+
+import javax.swing.JOptionPane;
+
 /*
  *    Copyright (c) The League of Amazing Programmers 2013-2019
  *    Level 1
@@ -11,14 +14,23 @@ public class RaceCarRunner {
 		/* Do the following things without changing the RaceCar class */
 		
 		// 1. Create a RaceCar and place it in 5th position
-	RaceCar cars1=new RaceCar("car1", 5); 
+	RaceCar cars1 = new RaceCar("car1", 5); 
 		// 2. Print the RaceCar's position in the race
-		System.out.println(5);
-		// 3. Crash the RaceCar
+		System.out.println(cars1.getPositionInRace());
+		// 3. Crash the RaceCar 
+		cars1.crash();
 		
 		// 4. If the car is damaged. Bring it in for a pit stop.
-
+		if(cars1.isDamaged()) {
+		cars1.pit();	
+		}
 		// 5. Help the car move into first place.
-
+		cars1.overtake();
+		cars1.overtake();
+		cars1.overtake();
+		cars1.overtake();
+		cars1.overtake();
+		cars1.overtake();
+		cars1.overtake();
 	}
 }
