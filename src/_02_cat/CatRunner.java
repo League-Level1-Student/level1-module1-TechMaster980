f591c61bf5fac36200d27d1291a6c7600f235644
TechMaster980 <37854546+TechMaster980@users.noreply.github.com>
2020-05-16 13:00:22 -0700

    finish one

--- src/_02_cat/CatRunner.java
+++ src/_02_cat/CatRunner.java
@@ -2,6 +2,13 @@ package _02_cat;
 
 public class CatRunner {
 public static void main(String[] args) {
+Cat cat = new Cat("pom");
+cat.meow();	
+cat.printName();
+for(int i = 0; i<=8; i++) {
+cat.kill();
+}
+}
 
 }
-}
+
