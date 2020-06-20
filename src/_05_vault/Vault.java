a9c2d92f0ba1aa3c961a8c4be288c02b49db962d
TechMaster980 <37854546+TechMaster980@users.noreply.github.com>
2020-06-20 12:56:49 -0700

    started one

---
+++ src/_05_vault/Vault.java
@@ -0,0 +1,16 @@
+package _05_vault;
+
+public class Vault {
+
+public static void main(String[] args) {
+if(tryCode() == true) {
+	
+}
+	
+}
+
+private static void tryCode() {
+	// TODO Auto-generated method stub
+	
+}
+}
---
+++ src/_06_duck/duck.java
@@ -0,0 +1,13 @@
+package _06_duck;
+
+public class duck {
+public static void main(String[] args) {
+	
+	Duck(String favoriteFood, int numberOfFriends) {
+        this.favoriteFood = favoriteFood;
+        this.numberOfFriends = numberOfFriends;
+ }
+	Duck daffy = new Duck("donuts",5)
+			daffy.quack(); 
+}
+}
