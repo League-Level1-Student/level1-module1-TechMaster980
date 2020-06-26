de58f06c2442bb5fb422091cdd483ddf56feeecb
TechMaster980 <37854546+TechMaster980@users.noreply.github.com>
2020-06-26 16:55:33 -0700

    still finishing it

--- src/_06_duck/duck.java
+++ src/_06_duck/duck.java
@@ -1,8 +1,10 @@
 package _06_duck;
 
+import javax.swing.JOptionPane;
+
 public class duck {
 public static void main(String[] args) {
-	
+	JOptionPane
 	Duck(String favoriteFood, int numberOfFriends) {
         this.favoriteFood = favoriteFood;
         this.numberOfFriends = numberOfFriends;
