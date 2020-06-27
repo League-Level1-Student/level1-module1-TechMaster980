3541e1729d003e1115c02591b37c0b89e69b5577
TechMaster980 <37854546+TechMaster980@users.noreply.github.com>
2020-06-27 12:58:23 -0700

    still trying to finish it

--- src/_06_duck/duck.java
+++ src/_06_duck/duck.java
@@ -4,11 +4,13 @@ import javax.swing.JOptionPane;
 
 public class duck {
 public static void main(String[] args) {
-	JOptionPane
+	string JOptionPane
 	Duck(String favoriteFood, int numberOfFriends) {
         this.favoriteFood = favoriteFood;
         this.numberOfFriends = numberOfFriends;
  }
+	
+	
 	Duck daffy = new Duck("donuts",5)
 			daffy.quack(); 
 }
