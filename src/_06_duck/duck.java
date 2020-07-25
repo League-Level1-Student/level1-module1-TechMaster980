53098aef33e2990f56317e535027ee8239eadcd8
henri <henri@DESKTOP-SF1O733>
2020-07-25 12:57:27 -0700

    added to this one

--- src/_06_duck/duck.java
+++ src/_06_duck/duck.java
@@ -4,9 +4,9 @@ import javax.swing.JOptionPane;
 
 public class duck {
 public static void main(String[] args) {
-	string JOptionPane = numberOfFriends
+	String numberOfFriends;
 	Object numberOfFriends;
-	Object numberOfFriends;
+	
 	Duck(String favoriteFood, int numberOfFriends) {
         Object favoriteFood;
 		this.favoriteFood = favoriteFood;
